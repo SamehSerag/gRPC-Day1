17e95feb33c0f2dcfc960f8b0e75cfda2633d4b6
Sameh Serag <36167068+SamehSerag@users.noreply.github.com>
2022-05-27 22:12:34 +0200

    first commit

---
+++ Gateway/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+//app.MapGrpcService<OrderServiceTest>();
+if(app.Environment.IsDevelopment())
+    app.MapGrpcReflectionService();
+app.MapControllers();
+
+app.Run();
